dab235cfe9fd7abf35c4ae19572a788f59aeabb0
Mikolaj Bajtkiewicz <oakdensity@pm.me>
2024-06-16 12:52:08 +0200

    refactor(flake): change to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A development shell for the Jari project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
