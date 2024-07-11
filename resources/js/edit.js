7363d222dcb4a8c87943b56b5495d6fe2955f001
Samir <samiryolo40@gmail.com>
2024-07-11 10:15:03 +0200

    refactor(js): add saving edit

---
+++ resources/js/edit.js
@@ -0,0 +1,6 @@
+function get_edit_content(){
+    var title = document.getElementById("title").value;
+    console.log(title);
+    var description = document.getElementById("description").value;
+    console.log(description)
+}
