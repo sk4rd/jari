dddcd9ec8271b081ab0a978fe823c516e494192d
Samir <samiryolo40@gmail.com>
2024-07-11 10:45:32 +0200

    fix(js): changed variables to not global

--- resources/js/edit.js
+++ resources/js/edit.js
@@ -1,6 +1,6 @@
 function get_edit_content(){
-    var title = document.getElementById("title").value;
+    let title = document.getElementById("title").value;
     console.log(title);
-    var description = document.getElementById("description").value;
+    let description = document.getElementById("description").value;
     console.log(description)
 }
