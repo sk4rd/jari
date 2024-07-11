38194995eeb099eca9850219cbcb46175e8eae6c
Samir <samiryolo40@gmail.com>
2024-07-11 11:11:11 +0200

    refactor(js): added submitting edit changes

--- resources/js/edit.js
+++ resources/js/edit.js
@@ -3,4 +3,7 @@ function get_edit_content(){
     console.log(title);
     let description = document.getElementById("description").value;
     console.log(description)
+    let rx = /([^\/]*)\/edit/g;
+    let id = rx.exec(document.URL)[1];
+    fetch("/" + id, {method:"POST", body: JSON.stringify({title, description}), headers: {"Content-Type":"application/json"}})
 }
