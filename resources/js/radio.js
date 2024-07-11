0eb03793c6fd069bd164f80749a607b528938025
Samir <samiryolo40@gmail.com>
2024-07-11 08:54:58 +0200

    refactor(html): add search function to start and radiopage

--- resources/js/radio.js
+++
@@ -1,7 +0,0 @@
-let radioname;
-
-document.getElementById("buttonRadiosuche").onClick = function(){
-        radioname = document.getElementById("search-input").value;
-        console.log(radioname);
-}
-
---
+++ resources/js/search.js
@@ -0,0 +1,5 @@
+function get_search_contents() {
+        var radioname = document.getElementById("search-input").value;
+        console.log(radioname);
+}
+
