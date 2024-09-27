04ba2af1edd620c7c689c63cef8c1a018e08031c
Aras14HD <aras14k5@gmail.com>
2024-09-27 10:46:41 +0200

    refactor(cli): added first args

--- src/cli.rs
+++ src/cli.rs
@@ -1,2 +1,16 @@
-pub enum Messages {}
-fn main() {}
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    RemoveSong { radio: String, song: String },
+    RemoveRadio { radio: String },
+}
+fn main() {
+    let args = Args::parse();
+}
