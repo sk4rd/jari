df707117915dd20055f5051dcb2a31dd0ac409da
Niklas <n.diehl@orange-p.de>
2024-11-03 18:07:26 +0100

    refactor(html): added Song Upload Button and Song Queue Managment

--- resources/js/settings.js
+++ resources/js/settings.js
@@ -32,3 +32,68 @@ function addRadio() {
         .then(data => console.log(data))
         .catch(error => console.error(error));
 }
+
+
+// settings.js
+
+const songQueue = [];
+
+function addSong() {
+    const fileInput = document.getElementById('music-file');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const songTitle = file.name; // Use the file name as the song title
+        songQueue.push({ title: songTitle, file: file });
+        updateSongQueue();
+        fileInput.value = ''; // Clear the file input
+    } else {
+        alert('Please select a music file.');
+    }
+}
+
+function removeSong(index) {
+    songQueue.splice(index, 1);
+    updateSongQueue();
+}
+
+function moveSongUp(index) {
+    if (index > 0) {
+        const song = songQueue.splice(index, 1)[0];
+        songQueue.splice(index - 1, 0, song);
+        updateSongQueue();
+    }
+}
+
+function moveSongDown(index) {
+    if (index < songQueue.length - 1) {
+        const song = songQueue.splice(index, 1)[0];
+        songQueue.splice(index + 1, 0, song);
+        updateSongQueue();
+    }
+}
+
+function updateSongQueue() {
+    const tbody = document.getElementById('song-queue').getElementsByTagName('tbody')[0];
+    tbody.innerHTML = ''; // Clear existing rows
+
+    songQueue.forEach((song, index) => {
+        const row = tbody.insertRow();
+        row.insertCell(0).innerText = song.title;
+        const actionCell = row.insertCell(1);
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'Remove';
+        removeButton.onclick = () => removeSong(index);
+        actionCell.appendChild(removeButton);
+
+        const moveUpButton = document.createElement('button');
+        moveUpButton.innerText = 'Move Up';
+        moveUpButton.onclick = () => moveSongUp(index);
+        actionCell.appendChild(moveUpButton);
+
+        const moveDownButton = document.createElement('button');
+        moveDownButton.innerText = 'Move Down';
+        moveDownButton.onclick = () => moveSongDown(index);
+        actionCell.appendChild(moveDownButton);
+    });
+}
